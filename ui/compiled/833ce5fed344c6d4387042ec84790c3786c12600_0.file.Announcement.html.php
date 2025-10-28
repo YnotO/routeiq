dc05345f9d24c7d8fee76a437e601415e42aa265
YnotO <tonyochieng65@gmail.com>
2025-10-28 18:32:04 +0300

    logo update

---
+++ ui/compiled/833ce5fed344c6d4387042ec84790c3786c12600_0.file.Announcement.html.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 4.5.3, created on 2025-10-28 18:26:33
+  from 'C:\xampp\htdocs\routeiq\pages\Announcement.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.3',
+  'unifunc' => 'content_6900e0a922d487_40824932',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '833ce5fed344c6d4387042ec84790c3786c12600' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\routeiq\\pages\\Announcement.html',
+      1 => 1761398593,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6900e0a922d487_40824932 (Smarty_Internal_Template $_smarty_tpl) {
+?>Pengumuman!!<br>Besok libur<br><br>Announcement!!<br>Tomorrow holiday<br><br>
+<br>
+This Announcement is for Login Page.<?php }
+}
